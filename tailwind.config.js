b29917da7b7f73605c2bef7d710a36d6b86dad1b
Zweli <zwelihle408@gmail.com>
2023-06-28 22:46:37 +0200

    Final build of my portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: ["var(--font-mont)", ...fontFamily.sans],
+      },
+      colors: {
+        primaryPurple:"#7F00FF",
+        electricPurple:"#8F00FF",
+        darkPurple:"#2D033B",
+        graySeven:"#121212",
+        blushPink:"#DE5D83",
+        mainFuchsia:"#d946ef",
+        safePink:"#F19EC8",
+
+      },
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+}
+
